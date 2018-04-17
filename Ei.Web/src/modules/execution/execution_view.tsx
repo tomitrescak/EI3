88491ff5872ef6ae0d5bad9390c880d3c92f7f79
Tomas Trescak <tomi.trescak@gmail.com>
2018-04-17 16:39:02 +1000

    Added examples for Avishka

--- Ei.Web/src/modules/execution/execution_view.tsx
+++ Ei.Web/src/modules/execution/execution_view.tsx
@@ -1,24 +1,29 @@
 import * as PIXI from 'pixi.js';
 import * as React from 'react';
+
+// AVISHKA
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+import { field, Form, FormState, Input } from 'semantic-ui-mobx';
 import { Button } from 'semantic-ui-react';
 
-// you need to create a model of agent
-class Agent {
-	x: number;
-  y: number;
+// // you need to create a model of agent
+// class Agent {
+// 	x: number;
+//   y: number;
   
-	constructor (x: number, y: number){
-    this.x = x;
-    this.y = y;
-  }
-	moveToLocation(x: number, y: number) {
-		return; 
-	}
-}
+// 	constructor (x: number, y: number){
+//     this.x = x;
+//     this.y = y;
+//   }
+// 	moveToLocation(x: number, y: number) {
+// 		return; 
+// 	}
+// }
 
 // test
-let agent = new Agent(10, 30);
-agent.moveToLocation(100, 300);
+// let agent = new Agent(10, 30);
+// agent.moveToLocation(100, 300);
 
 // then we need to also have a map of environment
 class EnvironmentObject {
@@ -31,20 +36,42 @@ class Project {
 }
 
 class Environment {
+  objects: { [index: string]: PIXI.Sprite } = {};
+  stage: PIXI.Container;
+
+  constructor(stage: PIXI.Container) {
+    this.stage = stage;
+  }
+
 	addObject(id: string, name: string, x: number, y:number) { 
     let blob = new PIXI.Sprite(PIXI.loader.resources['/images/blob.png'].texture);
     blob.name = 'Blob';
     blob.vy = 1;
+    blob.x = x;
+    blob.y = y;
+
+    // rememeber object for further manipulation
+    this.objects[id] = blob;
+
+    // add to canvas
+    this.stage.addChild(blob);
   }
-  addAgent(x: number, y: number) {
-    let cat = new PIXI.Sprite(PIXI.loader.resources['/images/explorer.png'].texture);
-    cat.name = 'cat';
-    cat.vy = 1;
-  }; 
+  
+  addAgent(id: string, x: number, y: number) {
+    let agent = new PIXI.Sprite(PIXI.loader.resources['/images/explorer.png'].texture);
+    agent.name = 'cat';
+    agent.vy = 1;
+
+    // rememeber object for further manipulation
+    this.objects[id] = agent;
+
+    // add to canvas
+    this.stage.addChild(agent);
+  }
+
   removeObject(id: string){
-    let deleteBlob = document.getElementById('Blob');
-    deleteBlob.removeChild(deleteBlob);
-  };
+    this.stage.removeChild(this.objects[id]);
+  }
 }
 
 
@@ -57,12 +84,12 @@ export class ExecutionView extends React.Component {
     return (
       <>
         <div ref={n => (this.canvas = n)} id="pixiCanvas" />
+        <ReactiveForm />
       </>
     );
   }
   componentDidMount() {
     let type = 'WebGL';
-    let cat: PIXI.Sprite;
     if (!PIXI.utils.isWebGLSupported()) {
       type = 'canvas';
     }
@@ -86,17 +113,57 @@ export class ExecutionView extends React.Component {
       name: 'Blob',
       image: '/images/blob.png'
     }];
-    let e = new Environment();
-    e.addObject('id1', 'Blob', 10, 10); // displays the object
-    e.removeObject('id1'); // removes the object
-    e.addAgent(20, 30); // adds agent */
+    
   }
 
   // CLASS METHODS
 
   setup = () => {
     let background = new PIXI.Sprite(PIXI.loader.resources['/images/dungeon.png'].texture);
-  // Add the cat to the stage
+    // Add the cat to the stage
     this.app.stage.addChild(background);
+
+    // add the rest
+
+    let e = new Environment(this.app.stage);
+    e.addObject('id1', 'Blob', 10, 10); // displays the object
+    // e.removeObject('id1'); // removes the object
+    e.addAgent('agent1', 20, 30); // adds agent */
+  }
+}
+
+///// AVISHKA
+
+class Reactive extends FormState {
+  @field testField = '';
+  @observable testArray: string[] = [];
+  
+  @field newArrayMember = '';
+}
+
+let data = new Reactive();
+
+@observer
+class ReactiveForm extends React.Component {
+
+  render() {
+    return (
+      <Form>
+        <Input owner={data.fields.testField} label="Test Text (.fields used only with owner)" />
+        <div style={{background: 'salmon'}}>{data.testField}</div>
+
+        <div>Let's do arrays</div>
+        { 
+          data.testArray.map((element, index) => (
+            <div key={index}>
+              <div >[{index}]: { element }</div>
+              <Button color="red" icon="trash" onClick={() => data.testArray.splice(index, 1)} />
+            </div>
+          )) 
+        }
+        <Input owner={data.fields.newArrayMember} label="New Member" />
+        <Button  icon="plus" color="blue" onClick={() => data.testArray.push(data.newArrayMember) } />
+      </Form>
+    )
   }
 }
